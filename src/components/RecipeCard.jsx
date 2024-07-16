26591d1d3d314cf4c9cefc7b9cc08792a95e4874
sonu-james <sonupjames@gmail.com>
2024-07-17 00:02:13 +0530

    updated

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,83 +1,99 @@
-import { faHeart, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
+import { faHeart, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import { Card } from 'react-bootstrap'
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { deleteRecipeApi } from '../../services/allApi';
 import { Link } from 'react-router-dom';
 
-function RecipeCard({ displayRecipe, setDeleteRecipeStatus, updateFavoriteCount, openEditModal }) {
+
+
+
+function RecipeCard({displayRecipe,setDeleteRecipeStatus}) {
+
   const [lgShow, setLgShow] = useState(false);
+    // const [show, setShow] = useState(false);
 
-  const handleDelete = async (id) => {
-    const result = await deleteRecipeApi(id);
-    if (result.status >= 200 && result.status < 300) {
-      setDeleteRecipeStatus(true);
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
+
+    const handleDelete=async(id)=>{
+      const result=await deleteRecipeApi(id)
+      console.log(result);
+      if(result.status >=200 && result.status <300){
+        setDeleteRecipeStatus(result.data)
+      }
+
     }
-
 
     // const PassRecipeId=(e,id)=>{
     //   console.log("clicked:",id);
     //   e.dataTransfer.setData("ID",id)
     // }
-  
-}
-
   return (
     <>
-      <Card style={{ width: '90%' }} className='my-5'>
-        <Card.Img variant="top" onClick={() => setLgShow(true)} src={displayRecipe?.RecipeImg} style={{height:'50vh',width:'100%'}}/>
-        <Card.Body>
-          <Card.Title>{displayRecipe?.RecipeName}</Card.Title>
-          <div className='d-flex justify-content-between'>
-            <Button variant="outline-primary" onClick={() => handleDelete(displayRecipe?.id)}>
-              <FontAwesomeIcon icon={faTrash} />
-            </Button>
-            <Button variant="outline-info" onClick={() => openEditModal(displayRecipe)}>
-              <FontAwesomeIcon icon={faPenToSquare} />
-            </Button>
-            <Button
-              variant="outline-success"
-              onClick={() => updateFavoriteCount(displayRecipe)}
-            >
-              <FontAwesomeIcon
-                icon={faHeart}
-                className={displayRecipe.isFavorite ? 'text-danger' : 'text-secondary'}
-              />
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
+    <Card style={{ width: '90%' }} className='my-5'>
+      <Card.Img variant="top" onClick={setLgShow} src={displayRecipe?.RecipeImg} width={'100%'} height={'300px'} className='p-3 rounded ' />
+      <Card.Body>
+        <Card.Title>{displayRecipe?.RecipeName}</Card.Title>
+        <Card.Text>
+       
+        </Card.Text>
+        <div className='d-flex justify-content-between'>
+        <Button variant="outline-primary me-3 w-25" onClick={()=>handleDelete(displayRecipe?.id)}><FontAwesomeIcon icon={faTrash} /></Button>{' '}
+        <Button variant="outline-info me-3 w-25"><Link to={`/EditRecipe/${displayRecipe?.id}`}><FontAwesomeIcon icon={faPenToSquare}  /></Link></Button>
+        {/* <Button onClick={(e)=>PassRecipeId(e,displayRecipe?.id)} variant="outline-info me-3 w-25"><Link to={`/EditRecipe`}><FontAwesomeIcon icon={faPenToSquare}  /></Link></Button> */}
+        <Button variant="outline-success w-25"><FontAwesomeIcon icon={faHeart} /></Button>{' '}
+    
+        </div>
+        
+      </Card.Body>
+    </Card>
 
-      <Modal size="lg" show={lgShow} onHide={() => setLgShow(false)}>
+
+    <Modal
+        size="lg"
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
         <Modal.Header closeButton>
-          <Modal.Title>{displayRecipe?.RecipeName}</Modal.Title>
+          <Modal.Title id="example-modal-sizes-title-lg">
+          {displayRecipe?.RecipeName}
+          <p className='fs-4'>Category:  {displayRecipe?.Category}</p>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="row">
             <div className="col-md-6">
-              <img src={displayRecipe?.RecipeImg} height={'300px'} width={'100%'} />
+              <img src={displayRecipe?.RecipeImg}  height={'300px'} width={'100%'} />
             </div>
             <div className="col-md-6">
               <h4>Cooking Instructions:</h4>
-              <p>{displayRecipe?.Description}</p>
-              <h6>Video Link: {displayRecipe?.url}</h6>
+              <p className='p-2' style={{textAlign:'justify'}}>{displayRecipe?.Description}
+                <h6 className='mt-3'> Video Link:{displayRecipe?.url}</h6>
+              </p>
             </div>
-            <div className="col-md-12">
-              <h4>Ingredients:</h4>
-              <p>{displayRecipe?.Ingredients}</p>
-              <iframe width="866" height="487" src={displayRecipe?.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            <div className="row">
+              <div className="col-md-12">
+              <h4>Ingrediants:</h4>
+              <p>
+              {displayRecipe?.Ingrediants}
+              </p>
+              <div className='p-2 ms-4'>
+              <iframe width="866" height="487" src={displayRecipe.url} title="CHICKEN TIKKA NO OVEN | CHICKEN TIKKA KEBAB ON TAWA | CHICKEN TIKKA RECIPE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              </div>
+              </div>
             </div>
           </div>
         </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => setLgShow(false)}>Close</Button>
-        </Modal.Footer>
       </Modal>
-    </>
+
     
-  );
+
+    </>
+  )
 }
 
-export default RecipeCard;
+export default RecipeCard
