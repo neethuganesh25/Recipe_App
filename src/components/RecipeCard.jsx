fb3d084cd410d58a824eb79b973b525b02c2cf92
Neethu Ganesh <96676287+neethuganesh25@users.noreply.github.com>
2024-07-17 13:38:16 +0530

    Update RecipeCard.jsx

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,89 +1,82 @@
-<<<<<<< HEAD
-import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faTrash, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
-=======
-import { faHeart, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
-import { Card } from 'react-bootstrap'
->>>>>>> 768be9348633ce2fa4b99f86dcc0ac7dee4e2762
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import { Card, Modal, Button } from 'react-bootstrap';
 import { deleteRecipeApi } from '../../services/allApi';
+import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
 function RecipeCard({ displayRecipe, setDeleteRecipeStatus, openEditModal }) {
-=======
-
-
-
-function RecipeCard({displayRecipe,setDeleteRecipeStatus}) {
-
->>>>>>> 768be9348633ce2fa4b99f86dcc0ac7dee4e2762
   const [lgShow, setLgShow] = useState(false);
-    // const [show, setShow] = useState(false);
 
-    // const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
-
-    const handleDelete=async(id)=>{
-      const result=await deleteRecipeApi(id)
-      console.log(result);
-      if(result.status >=200 && result.status <300){
-        setDeleteRecipeStatus(result.data)
-      }
-
+  const handleDelete = async (id) => {
+    const result = await deleteRecipeApi(id);
+    console.log(result);
+    if (result.status >= 200 && result.status < 300) {
+      setDeleteRecipeStatus(result.data);
     }
-<<<<<<< HEAD
   };
 
   return (
     <>
       <Card style={{ width: '90%', height: '60vh' }} className='my-5'>
-        <Card.Img variant="top" onClick={() => setLgShow(true)} src={displayRecipe?.RecipeImg} style={{ width: '100%', height: '90vh' }} />
+        <Card.Img
+          variant="top"
+          onClick={() => setLgShow(true)}
+          src={displayRecipe?.RecipeImg}
+          style={{ width: '100%', height: '90vh' }}
+        />
         <Card.Body>
           <Card.Title>{displayRecipe?.RecipeName}</Card.Title>
           <div className='d-flex justify-content-between'>
-            <Button variant="outline-primary" className='w-25' onClick={() => handleDelete(displayRecipe?.id)}>
+            <Button
+              variant="outline-primary"
+              className='w-25'
+              onClick={() => handleDelete(displayRecipe?.id)}
+            >
               <FontAwesomeIcon icon={faTrash} />
             </Button>
-            <Button variant="outline-info" className='w-25' onClick={() => openEditModal(displayRecipe)}>
+            <Button
+              variant="outline-info"
+              className='w-25'
+              onClick={() => openEditModal(displayRecipe)}
+            >
               <FontAwesomeIcon icon={faPenToSquare} />
             </Button>
           </div>
         </Card.Body>
       </Card>
-=======
 
-    // const PassRecipeId=(e,id)=>{
-    //   console.log("clicked:",id);
-    //   e.dataTransfer.setData("ID",id)
-    // }
-  return (
-    <>
-    <Card style={{ width: '90%' }} className='my-5'>
-      <Card.Img variant="top" onClick={setLgShow} src={displayRecipe?.RecipeImg} width={'100%'} height={'300px'} className='p-3 rounded ' />
-      <Card.Body>
-        <Card.Title>{displayRecipe?.RecipeName}</Card.Title>
-        <Card.Text>
-       
-        </Card.Text>
-        <div className='d-flex justify-content-between'>
-        <Button variant="outline-primary me-3 w-25" onClick={()=>handleDelete(displayRecipe?.id)}><FontAwesomeIcon icon={faTrash} /></Button>{' '}
-        <Button variant="outline-info me-3 w-25"><Link to={`/EditRecipe/${displayRecipe?.id}`}><FontAwesomeIcon icon={faPenToSquare}  /></Link></Button>
-        {/* <Button onClick={(e)=>PassRecipeId(e,displayRecipe?.id)} variant="outline-info me-3 w-25"><Link to={`/EditRecipe`}><FontAwesomeIcon icon={faPenToSquare}  /></Link></Button> */}
-        <Button variant="outline-success w-25"><FontAwesomeIcon icon={faHeart} /></Button>{' '}
-    
-        </div>
-        
-      </Card.Body>
-    </Card>
->>>>>>> 768be9348633ce2fa4b99f86dcc0ac7dee4e2762
+      <Card style={{ width: '90%' }} className='my-5'>
+        <Card.Img
+          variant="top"
+          onClick={() => setLgShow(true)}
+          src={displayRecipe?.RecipeImg}
+          width={'100%'}
+          height={'300px'}
+          className='p-3 rounded '
+        />
+        <Card.Body>
+          <Card.Title>{displayRecipe?.RecipeName}</Card.Title>
+          <div className='d-flex justify-content-between'>
+            <Button
+              variant="outline-primary me-3 w-25"
+              onClick={() => handleDelete(displayRecipe?.id)}
+            >
+              <FontAwesomeIcon icon={faTrash} />
+            </Button>
+            <Button variant="outline-info me-3 w-25">
+              <Link to={`/EditRecipe/${displayRecipe?.id}`}>
+                <FontAwesomeIcon icon={faPenToSquare} />
+              </Link>
+            </Button>
+            <Button variant="outline-success w-25">
+              <FontAwesomeIcon icon={faHeart} />
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
 
-
-    <Modal
+      <Modal
         size="lg"
         show={lgShow}
         onHide={() => setLgShow(false)}
@@ -91,26 +84,33 @@ function RecipeCard({displayRecipe,setDeleteRecipeStatus}) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="example-modal-sizes-title-lg">
-          {displayRecipe?.RecipeName}
-          <p className='fs-4'>Category:  {displayRecipe?.Category}</p>
+            {displayRecipe?.RecipeName}
+            <p className='fs-4'>Category: {displayRecipe?.Category}</p>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="row">
             <div className="col-md-6">
-              <img src={displayRecipe?.RecipeImg}  height={'300px'} width={'100%'} />
+              <img src={displayRecipe?.RecipeImg} height={'300px'} width={'100%'} />
             </div>
             <div className="col-md-6">
               <h4>Cooking Instructions:</h4>
-              <p className='p-2' style={{textAlign:'justify'}}>{displayRecipe?.Description}
-                <h6 className='mt-3'> Video Link:{displayRecipe?.url}</h6>
+              <p className='p-2' style={{ textAlign: 'justify' }}>
+                {displayRecipe?.Description}
+                <h6 className='mt-3'> Video Link: {displayRecipe?.url}</h6>
               </p>
             </div>
-<<<<<<< HEAD
             <div className="col-md-12">
               <h4>Ingredients:</h4>
               <p>{displayRecipe?.Ingredients}</p>
-              <iframe width="760" height="487" src={displayRecipe?.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+              <iframe
+                width="760"
+                height="487"
+                src={displayRecipe?.url}
+                frameBorder="0"
+                allow="autoplay; encrypted-media"
+                allowFullScreen
+              ></iframe>
             </div>
           </div>
         </Modal.Body>
@@ -123,27 +123,3 @@ function RecipeCard({displayRecipe,setDeleteRecipeStatus}) {
 }
 
 export default RecipeCard;
-=======
-            <div className="row">
-              <div className="col-md-12">
-              <h4>Ingrediants:</h4>
-              <p>
-              {displayRecipe?.Ingrediants}
-              </p>
-              <div className='p-2 ms-4'>
-              <iframe width="866" height="487" src={displayRecipe.url} title="CHICKEN TIKKA NO OVEN | CHICKEN TIKKA KEBAB ON TAWA | CHICKEN TIKKA RECIPE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-              </div>
-              </div>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
-
-    
-
-    </>
-  )
-}
-
-export default RecipeCard
->>>>>>> 768be9348633ce2fa4b99f86dcc0ac7dee4e2762
